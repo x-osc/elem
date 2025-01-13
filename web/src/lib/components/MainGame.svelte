5be7830d21efc445b5b2316846996aeae6cd6935
20dinosaurs <maxymcculloch@gmail.com>
2025-01-13 23:35:47 +1300

    reactive list test

---
+++ web/src/lib/components/MainGame.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  type ElementState = { [key: string]: string[] };
+
+  const elementState: ElementState = $state({
+    air: ["air"],
+  });
+</script>
+
+<div>
+  {#each elementState["air"] as element}
+    <div>air</div>
+  {/each}
+
+  <button onclick={() => elementState["air"].push("air")}>big er</button>
+</div>
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import MainGame from "$lib/components/MainGame.svelte";
+</script>
+
+<svelte:head>
+  <title>elm ent</title>
+</svelte:head>
+
+<MainGame />
