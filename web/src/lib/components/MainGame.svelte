7fa79a12025487ec44ecaf6c60bfc7db923b17c4
20dinosaurs <maxymcculloch@gmail.com>
2025-01-14 22:05:53 +1300

    list categories

--- web/src/lib/components/MainGame.svelte
+++ web/src/lib/components/MainGame.svelte
@@ -3,12 +3,17 @@
 
   const elementState: ElementState = $state({
     air: ["air"],
+    earth: ["earth"],
+    fire: ["fire"],
+    water: ["water"],
   });
+
+  const categories = $derived(Object.keys(elementState));
 </script>
 
 <div>
-  {#each elementState["air"] as element}
-    <div>air</div>
+  {#each categories as category}
+    <div>{category}</div>
   {/each}
 
   <button onclick={() => elementState["air"].push("air")}>big er</button>
