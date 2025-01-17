774f0a1e1060978440d60372dc2a5fef2cdfb4ee
20dinosaurs <maxymcculloch@gmail.com>
2025-01-17 23:47:42 +1300

    click to pick

--- web/src/lib/components/Element.svelte
+++ web/src/lib/components/Element.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import { getContext } from "svelte";
   import ElementDisplay from "./ElementDisplay.svelte";
 
   let { id }: { id: string } = $props();
+
+  let onClickCallback: (id: string) => void = getContext("onClickCallback");
 </script>
 
-<div><ElementDisplay name={id} /></div>
+<div onclick={() => onClickCallback(id)}><ElementDisplay name={id} /></div>
 
 <style>
   div {
--- web/src/lib/components/MainGame.svelte
+++ web/src/lib/components/MainGame.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import { setContext } from "svelte";
   import Category from "./Category.svelte";
   import HeldElement from "./HeldElement.svelte";
+
+  // game state
 
   type ElementState = { [key: string]: string[] };
 
@@ -15,6 +18,16 @@
     elementState[category].push(id);
   }
 
+  // held element
+
+  let heldElement: string | null = $state(null);
+
+  function onElementClicked(id: string) {
+    heldElement = id;
+  }
+
+  setContext("onClickCallback", onElementClicked);
+
   const categories = $derived(Object.keys(elementState));
 </script>
 
@@ -23,7 +36,9 @@
     <Category id={categoryId} elements={elementState[categoryId]} />
   {/each}
 
-  <HeldElement id="air" />
+  {#if heldElement}
+    <HeldElement id={heldElement} />
+  {/if}
 
   <button onclick={() => addElement("air", "air")}>big er</button>
 </div>
