580a4acda0e7a6ba185f5a7f70659f148c19986a
20dinosaurs <maxymcculloch@gmail.com>
2025-01-19 20:08:20 +1300

    element data

--- web/src/lib/utils/data.ts
+++ web/src/lib/utils/data.ts
@@ -1,6 +1,15 @@
 import combinationsJson from "$lib/data/combinations.json";
+import elementsJson from "$lib/data/elements.json";
+
+export type ElementData = {
+  name: string;
+  category: string;
+  tier: number;
+  color: string;
+};
 
 const combinations: { [comb: string]: string } = combinationsJson;
+const elements: { [id: string]: ElementData } = elementsJson;
 
 export function getResult(elem1: string, elem2: string): string | null {
   const comb1 = combinations[elem1 + "|" + elem2];
@@ -13,4 +22,8 @@ export function getResult(elem1: string, elem2: string): string | null {
   } else {
     return null;
   }
+}
+
+export function getElementData(id: string): ElementData | null {
+  return elements[id] ?? null;
 }
