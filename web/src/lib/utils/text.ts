0159f052e4b2d1ff17e08cfd394f88a2dd84cd3f
20dinosaurs <maxymcculloch@gmail.com>
2025-01-19 21:55:45 +1300

    adjust lightness threshold

--- web/src/lib/utils/text.ts
+++ web/src/lib/utils/text.ts
@@ -3,5 +3,5 @@ export function colorIsDark(bgColor: string) {
   const r = parseInt(color.substring(0, 2), 16);
   const g = parseInt(color.substring(2, 4), 16);
   const b = parseInt(color.substring(4, 6), 16);
-  return r * 0.299 + g * 0.587 + b * 0.114 <= 149;
+  return r * 0.299 + g * 0.587 + b * 0.114 <= 161;
 }
