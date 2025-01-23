a1360f6cd807c0b120676f8240390ced4984cbaa
20dinosaurs <maxymcculloch@gmail.com>
2025-01-23 23:17:36 +1300

    rewrite the frontend to be faster aswell lol im dumb

--- web/src/lib/utils/data.ts
+++ web/src/lib/utils/data.ts
@@ -19,15 +19,16 @@ const elements: { [id: string]: ElementData } = elementsJson;
 
 export function getResult(elem1: string, elem2: string): string | null {
   const comb1 = combinations[elem1 + "|" + elem2];
-  const comb2 = combinations[elem2 + "|" + elem1];
-
   if (comb1) {
     return comb1;
-  } else if (comb2) {
-    return comb2;
-  } else {
-    return null;
   }
+
+  const comb2 = combinations[elem2 + "|" + elem1];
+  if (comb2) {
+    return comb2;
+  }
+
+  return null;
 }
 
 export function getElementData(id: string): ElementData | null {
