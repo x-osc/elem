9d41ba57f9f525dc94b57936d09ace31c87c791b
20dinosaurs <maxymcculloch@gmail.com>
2025-01-27 01:33:32 +1300

    parse mutiple meta props

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,13 +1,18 @@
 use winnow::{
-    ascii::{self, space0, till_line_ending},
-    combinator::{opt, preceded},
+    ascii::{self, space0},
+    combinator::{opt, preceded, repeat, terminated},
     prelude::*,
 };
+
+fn elem_parser<'s>(input: &mut &'s str) -> PResult<Vec<(&'s str, &'s str)>> {
+    let _ = ascii::multispace0.parse_next(input)?;
+    repeat(0.., terminated(prop_val, opt(ascii::multispace0))).parse_next(input)
+}
 
 fn prop_val<'s>(input: &mut &'s str) -> PResult<(&'s str, &'s str)> {
     let prop = preceded("@", ascii::alphanumeric1).parse_next(input)?;
     let _ = (opt(space0), ":", opt(space0)).parse_next(input)?;
-    let val = till_line_ending.parse_next(input)?;
+    let val = ascii::till_line_ending.parse_next(input)?;
 
     Ok((prop, val))
 }
@@ -16,5 +21,5 @@ fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
     let mut data = src.as_str();
 
-    println!("{:?}", prop_val.parse_next(&mut data).unwrap())
+    println!("{:?}", elem_parser.parse(data).unwrap())
 }
