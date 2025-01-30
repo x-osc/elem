cb9fd81b6a7af34457aeacb16bb07a96762082f7
20dinosaurs <maxymcculloch@gmail.com>
2025-01-30 19:05:45 +1300

    support single line comments

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,8 +1,8 @@
 use winnow::{
-    ascii::{self, line_ending, space0},
-    combinator::{alt, eof, opt, preceded, repeat, repeat_till, terminated},
+    ascii::{self, space0},
+    combinator::{alt, delimited, opt, preceded, repeat},
     prelude::*,
-    token::{any, take, take_till},
+    token::take_till,
 };
 
 #[derive(Debug)]
@@ -15,14 +15,9 @@ fn comment(input: &mut &str) -> PResult<()> {
 }
 
 fn take_till_end_or_comment<'s>(input: &mut &'s str) -> PResult<&'s str> {
-    let val = take_till(1.., ['#', '\n', '\r']).parse_next(input)?;
+    let val = take_till(1.., ['#', '\n', '\r']).parse_next(input)?; // dont consume #
     let _ = opt(comment).parse_next(input);
     Ok(val.trim())
-}
-
-fn parse_input(input: &mut &str) -> PResult<Vec<Stmt>> {
-    let _ = ascii::multispace0.parse_next(input)?;
-    repeat(0.., terminated(prop_val, opt(ascii::multispace0))).parse_next(input)
 }
 
 fn prop_val(input: &mut &str) -> PResult<Stmt> {
@@ -34,6 +29,31 @@ fn prop_val(input: &mut &str) -> PResult<Stmt> {
         prop: prop.to_string(),
         val: val.to_string(),
     })
+}
+
+fn parse_stmt(input: &mut &str) -> PResult<Stmt> {
+    prop_val.parse_next(input)
+}
+
+fn parse_input(input: &mut &str) -> PResult<Vec<Stmt>> {
+    // skip initial whitespace
+    let _ = ascii::multispace0.parse_next(input)?;
+
+    // parse stmts
+    repeat(
+        0..,
+        delimited(
+            ascii::multispace0,
+            alt((
+                comment.map(|_| None), // parse and ignore comments
+                parse_stmt.map(Some),
+            )),
+            ascii::multispace0,
+        ),
+    )
+    // filter None values (comments)
+    .map(|stmts: Vec<Option<Stmt>>| stmts.into_iter().flatten().collect())
+    .parse_next(input)
 }
 
 fn main() {
