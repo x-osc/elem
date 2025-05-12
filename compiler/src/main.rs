77361db324a83c31c7b1686daae00a054df90678
x-osc <urlocaldumbass@proton.me>
2025-05-13 11:45:23 +1200

    add conversion to json format from parsed syntax

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,8 +1,11 @@
 use std::{env::args, fs::read_to_string};
 
 use parser::parse_str;
+use save::stmts_to_json;
 
+mod color;
 mod parser;
+mod save;
 
 fn main() {
     let Some(file_path) = args().nth(1) else {
@@ -14,8 +17,21 @@ fn main() {
         return;
     };
 
-    match parse_str(&src) {
-        Ok(res) => println!("{res:#?}"),
-        Err(err) => println!("{err}"),
-    }
+    let stmts = match parse_str(&src) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("{err}");
+            return;
+        }
+    };
+
+    let json_formatted = match stmts_to_json(stmts) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("{err}");
+            return;
+        }
+    };
+    
+    println!("{json_formatted:#?}")
 }
